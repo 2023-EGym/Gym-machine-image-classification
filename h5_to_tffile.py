4b71c9b81ac7c638f46af6e63d0612820c55e418
Jeonhyeryung <61443621+Jeonhyeryung@users.noreply.github.com>
2023-07-28 06:09:02 +0900

    Update h5_to_tffile.py

--- h5_to_tffile.py
+++ h5_to_tffile.py
@@ -1,5 +1,13 @@
+import argparse
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+
+def get_args_parser():
+    # define the argparse for the script
+    parser = argparse.ArgumentParser('Training setting', add_help=False)
+    parser.add_argument('--save_path', type=str, help='save path')
+    parser.add_argument('--model_path', type=str, help='root path of saved model')
+
 
 def make_tffile(model_path, save_path):
     model = load_model(model_path)
@@ -11,3 +19,10 @@ def make_tffile(model_path, save_path):
     # Save the TensorFlow Lite model to a file
     with open(save_path, 'wb') as f:
         f.write(tflite_model)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser('make tffile....', parents=[get_args_parser()])
+    args = parser.parse_args()
+
+    make_tffile(args.model_path, args.save_path)
