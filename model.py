a764dd2386c1858fc2cfaf373b3f695535fff8b1
Jeonhyeryung <61443621+Jeonhyeryung@users.noreply.github.com>
2023-08-14 12:18:56 +0900

    Update model.py

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
+from vit_keras import vit
 
 
 def model_1(num_classes, input_shape = (224, 224, 3)):
@@ -65,3 +66,51 @@ def model_3(num_classes, input_shape = (224, 224, 3)):
         layer.trainable = False
         
     return model 
+
+def teacher_model(num_classes, input_shape = (224, 224, 3)):
+    vit_model = vit.vit_l16(
+        image_size=img_height,
+        activation='softmax',
+        pretrained=True,
+        include_top=False,
+        pretrained_top=False
+    )
+    
+    inputs = tf.keras.Input(shape=(img_height, img_width, 3))
+    x = vit_model(inputs)
+    embedding = layers.Dense(128)(x)
+    
+    classifier_output = layers.Dense(num_classes, activation="softmax")(embedding)
+    
+    model = Model(inputs, classifier_output)
+
+    beta_1 = 0.9
+    beta_2 = 0.999
+    adam_optimizer = Adam(learning_rate=1e-4, beta_1=beta_1, beta_2=beta_2)
+    
+    model.compile(optimizer=adam_optimizer,
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    return model
+
+def student_model(num_classes, input_shape = (224, 224, 3)):
+    # Create the student model using ResNet-50
+    student_inputs = tf.keras.Input(shape=(img_height, img_width, 3))
+    student_resnet_model = ResNet50(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
+
+    student_x = student_resnet_model(student_inputs)
+    student_x = GlobalAveragePooling2D()(student_x)
+    student_embedding = Dense(256, activation='relu', kernel_initializer='he_normal')(student_x)
+    student_predictions = Dense(num_classes, activation="softmax")(student_embedding)
+    student_model = Model(student_inputs, student_predictions)
+    
+    beta_1 = 0.9
+    beta_2 = 0.999
+    adam_optimizer = Adam(learning_rate=1e-4, beta_1=beta_1, beta_2=beta_2)
+    
+    student_model.compile(optimizer=adam_optimizer,
+                          loss='categorical_crossentropy',
+                          metrics=['accuracy'])
+
+    return student_model
